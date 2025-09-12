8a642e7126edd06a69f20e7a19553dd1afcde4f4
Nicklas Lyckman <nlyckman@kth.se>
2025-09-12 13:32:59 +0200

    q4 complete

--- q4/count_1.sv
+++ q4/count_1.sv
@@ -2,7 +2,7 @@ module count_1 (
   input logic [3:0] a,
   output logic [2:0] out
 );
-  // …
-  // Add your description here
-  // …
+
+assign out = a[3] + a[2] + a[1] + a[0];
+
 endmodule
--- q4/count_1_tb.sv
+++ q4/count_1_tb.sv
@@ -6,5 +6,26 @@ module count_1_tb;
   // Outputs
   logic [2:0] out;
 
-  // complete
+  count_1 uut(
+    .a(a),
+    .out(out)
+  );
+
+  initial begin
+    int out_tmp;
+    for (int i; i<16; i++) begin
+      a = i;
+      #5ns;
+
+      if (out == a[3] + a[2] + a[1] + a[0]) begin
+        $display("PASSED for a = %b, out = %b", a, out);
+      end else begin
+        out_tmp = a[3] + a[2] + a[1] + a[0];
+        $display("FAILED for a = %b, out = %b, EXPECTED out = %b",
+                  a, out, out_tmp);
+      end
+    end
+    $finish;
+  end
+
 endmodule
