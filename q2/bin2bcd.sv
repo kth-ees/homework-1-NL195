bbf7ecd83bb6c988c390ba33a407f7f6efd1e843
Nicklas Lyckman <nlyckman@kth.se>
2025-09-12 11:52:10 +0200

    q2 done

--- q2/bin2bcd.sv
+++ q2/bin2bcd.sv
@@ -3,7 +3,30 @@ module bin2bcd (
   output logic [3:0] bcd,
   output logic carry
 );
-  // …
-  // Add your description here
-  // …
+
+always_comb begin
+  // Default statement to avoid latches
+  {carry, bcd} = 5'b0;
+
+  case(binary)
+  4'b0000: {carry, bcd} = 5'b00000; 
+  4'b0001: {carry, bcd} = 5'b00001;
+  4'b0010: {carry, bcd} = 5'b00010;
+  4'b0011: {carry, bcd} = 5'b00011;
+  4'b0100: {carry, bcd} = 5'b00100; 
+  4'b0101: {carry, bcd} = 5'b00101;
+  4'b0110: {carry, bcd} = 5'b00110;
+  4'b0111: {carry, bcd} = 5'b00111;
+  4'b1000: {carry, bcd} = 5'b01000; 
+  4'b1001: {carry, bcd} = 5'b01001;
+  4'b1010: {carry, bcd} = 5'b10000; 
+  4'b1011: {carry, bcd} = 5'b10001;
+  4'b1100: {carry, bcd} = 5'b10010; 
+  4'b1101: {carry, bcd} = 5'b10011;
+  4'b1110: {carry, bcd} = 5'b10100;
+  4'b1111: {carry, bcd} = 5'b10101; 
+  endcase
+end
+
+
 endmodule
--- q2/bin2bcd_tb.sv
+++ q2/bin2bcd_tb.sv
@@ -5,5 +5,45 @@ module bin2bcd_tb;
   logic [3:0] bcd;
   logic carry;
 
-  // complete your tb
+  bin2bcd uut(
+    .binary(binary),
+    .bcd(bcd),
+    .carry(carry)
+  );
+
+  initial begin
+    int j;
+
+    $dumpfile("waveform.vcd");
+    $dumpvars(0, uut);
+
+    for (int i; i<10; i++) begin
+      binary = i;
+      #10ns;
+
+      if ((bcd == binary)&&(carry == 1'b0)) begin
+        $display("PASS for binary = %b, carry = %b, bcd = %b", binary, carry, bcd);
+      end else begin
+        $display("FAIL for binary = %b, carry = %b, bcd = %b. EXPECTED: carry = %b, bcd = %b", binary, carry, bcd, 1'b0, binary);
+      end
+    end
+
+    j = 0;
+    for (int i = 10; i<16; i++) begin
+      binary = i;
+      #10ns;
+
+      if ((bcd == j)&&(carry == 1'b1)) begin
+        $display("PASS for binary = %b, carry = %b, bcd = %b", binary, carry, bcd);
+      end else begin
+        $display("FAIL for binary = %b, carry = %b, bcd = %b. EXPECTED: carry = %b, bcd = %b", binary, carry, bcd, 1'b1, j);
+      end
+      
+      j++;
+    end
+
+    $finish;
+
+  end
+
 endmodule
