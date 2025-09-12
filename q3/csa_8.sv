690523ba2ba584f592b791ab21ae2082cb783ccf
Nicklas Lyckman <nlyckman@kth.se>
2025-09-12 13:12:06 +0200

    q3 complete

--- q3/csa_8.sv
+++ q3/csa_8.sv
@@ -3,7 +3,29 @@ module csa_8 (
   output logic [7:0] sum,
   output logic carry
 );
-  // …
-  // Add your description here
-  // …
+
+logic c0, c1, c2;
+logic [3:0] s0, s1, s2;
+
+adder_4 adder0(
+  .A(a[3:0]),
+  .B(b[3:0]),
+  .sum(s0),
+  .carry(c0)
+  );
+
+adder_4 adder1(
+  .A(a[7:4]),
+  .B(b[7:4]),
+  .sum(s1),
+  .carry(c1)
+);
+
+assign {c2, s2} = a[7:4] + b[7:4] + 1;
+
+assign sum[3:0] = s0;
+assign sum[7:4] = c0 ? s2 : s1;
+assign carry = c0 ? c2 : c1;
+
+
 endmodule
--- q3/csa_8_tb.sv
+++ q3/csa_8_tb.sv
@@ -3,9 +3,41 @@ module csa_8_tb;
   // Inputs
   logic [7:0] a;
   logic [7:0] b;
-  logic [7:0] sum,
+  logic [7:0] sum;
   logic carry;
 
-  // complete
+csa_8 uut(
+  .a(a),
+  .b(b),
+  .sum(sum),
+  .carry(carry)
+);
+
+initial begin
+  int total;
+
+  $dumpfile("waveform.vcd");
+  $dumpvars(0, uut);
+
+  for(int i; i<2**8; i++) begin
+    a = i;
+    for(int j; j<2**8; j++) begin
+      b = j;
+
+      #5ns;
+
+      if ({carry, sum} == (a + b)) begin
+        continue;
+      end else begin
+        total = a + b;
+        $display("FAILED: a = %b, b = %b, carry = %b, sum = %b, EXPECTED: carry = %b, sum = %b", 
+                  a, b, carry, sum, total[8], total[7:0]);
+      end
+    end
+  end
+  $display("Test complete. If no FAILED message, all tests passed.");
+  $finish;      
+
+end
   
 endmodule
