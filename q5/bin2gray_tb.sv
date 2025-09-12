47a73816b2eb23f6cce253f4cf934e67e3a6b7e9
Nicklas Lyckman <nlyckman@kth.se>
2025-09-12 14:03:48 +0200

    q5 complete

--- q5/bin2gray_tb.sv
+++ q5/bin2gray_tb.sv
@@ -6,5 +6,27 @@ module bin2gray_tb;
   // Outputs
   logic [3:0] gray;
 
-  // complete
+  bin2gray uut(
+    .binary(binary),
+    .gray(gray)
+  );
+
+  initial begin
+    logic [3:0] expected_gray;
+
+    for (int i; i<8; i++) begin
+      binary = i;
+      #5ns;
+
+      expected_gray = binary ^ (binary >> 1);
+
+      if (gray === expected_gray) begin
+        $display("PASS: bin=%b, gray=%b", binary, gray);
+      end else begin
+        $display("FAIL: bin=%b, gray=%b, EXPECTED=%b", binary, gray, expected_gray);
+      end
+    end
+    $finish;
+  end
+
 endmodule
